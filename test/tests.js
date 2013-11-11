7861a0b5a53ada64c112a572587966daca52d516
Ron Waldon <jokeyrhyme@gmail.com>
2013-11-11 12:39:44 +1100

    support space-delimited rel-types and unquoted rel-types

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,9 @@
 var parseLinks = require('../lib');
 var fixture = '</api/users?page=0&per_page=2>; rel="first", ' +
               '</api/users?page=1&per_page=2>; rel="next", ' +
-              '</api/users?page=3&per_page=2>; rel="last"';
+              '</api/users?page=3&per_page=2>; rel="last", ' +
+              '</api/users/123>;rel=self, ' +
+              '</api/users/12345>;rel="related alternate"';
 
 describe('parse-links', function () {
   it('parse the links!', function () {
@@ -9,5 +11,8 @@ describe('parse-links', function () {
     parsed.first.should.eql('/api/users?page=0&per_page=2');
     parsed.next.should.eql('/api/users?page=1&per_page=2');
     parsed.last.should.eql('/api/users?page=3&per_page=2');
+    parsed.self.should.eql('/api/users/123');
+    parsed.alternate.should.eql('/api/users/12345');
+    parsed.related.should.eql('/api/users/12345');
   });
 });
